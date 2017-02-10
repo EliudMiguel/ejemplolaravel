183f0fc8aeddc2e0c40cfb454ecbe9938ef9cb82
Eliud Miguel <miguelantonio.e1@gmail.com>
2017-02-09 18:21:34 -0600

    Again

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -75,7 +75,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel--Eliud Miguel Antonio estuvo aqui nuevamente
                 </div>
 
                 <div class="links">
